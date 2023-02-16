a1275c53bddca1aa71389eefd008d0099021453c
andrey <tirtg@yandex.ru>
2023-02-16 09:00:57 +0300

    change code for write in file random row a or b

--- homework_2/task_1.java
+++ homework_2/task_1.java
@@ -1,29 +1,28 @@
 package homework_2;
 
-import java.util.Random;
 /**
  * task_1
  * 
  */
+import java.util.Random;
 import java.util.Scanner;// Ипортируем метод для считывания данных с консоли
 import java.io.File; // Импортируем метод для работы с файлом
-// import java.io.IOException;
 import java.io.FileWriter;
-// import java.io.Writer;
 import java.lang.reflect.Array;
+// import java.io.IOException;
+// import java.io.Writer;
 
 public class task_1 {
     private static Scanner input = new Scanner(System.in);// через input.nextInt(); будем считывать данные
 
     public static void main(String[] args) {
-        // boolean d = newFile.createNewFile();
-        // File newFile = new File("22.txt");
-
         int numberA = getNumberByUser("Введите число которое хотите возвести в степень");
-        int numberB = getNumberByUser("Введите  степень в которую хотите возвести число " + numberA);
+        int numberB = getNumberByUser("Введите степень в которую хотите возвести число " + numberA);
         System.out.println("Вы ввели а = " + numberA + ", b = " + numberB);
         String[] data = convertInput(numberA, numberB);
+        // String[] randomData = getRandomElement(data);
         WriteData(data);
+
     }
 
     /**
@@ -39,7 +38,7 @@ public class task_1 {
     }
 
     /**
-     * Функция записывает данны из ввода пользователя
+     * Функция записывает данные из ввода пользователя
      * 
      * @param numA то что возводим в степень
      * @param numB в какую степень возводим
@@ -55,7 +54,7 @@ public class task_1 {
                     System.out.println(string + "\n");
                     writer.write(string);
                 }
-                writer.flush();
+                // writer.flush();
 
             } else {
                 System.out.println("the file contains the valuse 'a' and 'b'. File overwritten");
@@ -73,12 +72,25 @@ public class task_1 {
         return newFile;
     }
 
+    public static int getRandomElement() {
+        int numRandom = new Random().nextInt(7);
+        return numRandom;
+    }
+
     public static String[] convertInput(int numA, int numB) {
         String aNum = Integer.toString(numA);
         String bNum = Integer.toString(numB);
         String row1 = "a " + aNum;
         String row2 = "b " + bNum;
-        String[] arrayRow = new String[] { row1, row2 };
+        int rand = getRandomElement();
+        String[] arrayRow = new String[2];
+        if (rand < 3) {
+            arrayRow[0] = row1;
+            arrayRow[1] = row2;
+        } else {
+            arrayRow[1] = row1;
+            arrayRow[0] = row2;
+        }
         return arrayRow;
     }
 }
